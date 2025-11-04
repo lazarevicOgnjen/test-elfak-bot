4e90cb98bf9bac4eefd617d2988aad1030cf0aad
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-11-04 22:01:27 +0100

    Update email.py

--- email.py
+++ email.py
@@ -1,31 +1,25 @@
-import os
-import json
 import gspread
 
-credentials_json = os.environ.get("GOOGLE_CREDENTIALS_JSON")
-if not credentials_json:
-    raise RuntimeError("GOOGLE_CREDENTIALS_JSON not set")
-
-credentials_dict = json.loads(credentials_json)
-
-gc = gspread.service_account_from_dict(credentials_dict)
-
-SPREADSHEET_NAME = "Elfak bot"
-sh = gc.open(SPREADSHEET_NAME)
+SPREADSHEET_URL = "YOUR_SHEET_URL"
 
 subjects = ["sip","bp","lp","sp","aor1","oop","oopj","pj","dmat","aip","uur"]
 
+sh = gspread.public().open_by_url(SPREADSHEET_URL)
+
 for subj in subjects:
     try:
+        
         ws = sh.worksheet(subj)
+
+        
+        emails = [e.strip() for e in ws.col_values(1) if e.strip()]
+
+        
+        filename = f"{subj}_emails.md"
+        with open(filename, "w") as f:
+            f.write("\n".join(emails))
+
+        print(f"✅ {len(emails)} emails saved to {filename}")
+
     except gspread.exceptions.WorksheetNotFound:
         print(f"⚠️ Sheet '{subj}' not found, skipping...")
-        continue
-
-    emails = [e.strip() for e in ws.col_values(1) if e.strip()]
-
-    filename = f"{subj}_emails.md"
-    with open(filename, "w") as f:
-        f.write("\n".join(emails))
-
-    print(f"✅ {len(emails)} emails saved to {filename}")
