1bbfd9131d4fabc6c89a81ef7ab8e83f8df618d3
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-11-05 23:20:09 +0100

    Update sip.py

--- sip.py
+++ sip.py
@@ -4,21 +4,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
 
-# Chrome options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.binary_location = "/usr/bin/google-chrome"
 
-# Chrome driver setup
 browser_driver = Service('/usr/bin/chromedriver')
 
-# Start the browser
 page_to_scrape = webdriver.Chrome(service=browser_driver, options=chrome_options)
 
 try:
-    # Step 1: Navigate to the page
+    
     page_to_scrape.get("https://sip.elfak.ni.ac.rs")
     time.sleep(3)
 
@@ -44,5 +41,5 @@ try:
     responseT.screenshot('sip.png')
 
 finally:
-    # Close the browser
+   
     page_to_scrape.quit()
