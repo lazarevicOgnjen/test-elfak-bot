518216167bc14e6bfcecabb953abf2b11f24784c
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-11-05 22:34:48 +0100

    Update cs_elfak.py

--- cs_elfak.py
+++ cs_elfak.py
@@ -101,7 +101,7 @@ try:
     responseLP.screenshot('lp.png')
 
     # oopj
-    page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=62&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2025&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=")
+    page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=62&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2023&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=")
     time.sleep(3)  
     responseOOPJ = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
     novosti_markdownOOPJ = responseOOPJ.text
